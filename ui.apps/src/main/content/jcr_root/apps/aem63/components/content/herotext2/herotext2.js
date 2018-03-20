2cd589553958415e93f355493e71b04b9f7f196c
Kunal <Kunal@Kunal-HP>
2018-03-20 17:55:26 +0530

    cc

--- ui.apps/src/main/content/jcr_root/apps/aem63/components/content/herotext2/herotext2.js
+++ ui.apps/src/main/content/jcr_root/apps/aem63/components/content/herotext2/herotext2.js
@@ -1,23 +1,18 @@
 "use strict";
 use(["/libs/wcm/foundation/components/utils/AuthoringUtils.js"], function (AuthoringUtils) {
     var CONST = {
-       PROP_TITLE: "jcr:title",
-	PROP_DESCRIPTION: "jcr:description",
+       PROP_TITLE: "jcr:Heading",
+	PROP_DESCRIPTION: "jcr:description"
     }
-    
+
     var hero = {}
     
     //Get the title text
     hero.text = properties.get(CONST.PROP_TITLE)
-            || pageProperties.get(CONST.PROP_TITLE)
-            || currentPage.name;
+            || pageProperties.get(CONST.PROP_TITLE);
 
      hero.description = properties.get(CONST.PROP_DESCRIPTION)
             || pageProperties.get(CONST.PROP_DESCRIPTION);
 
-
-    
-    
-    
-    return hero;
+	return hero;
 });
