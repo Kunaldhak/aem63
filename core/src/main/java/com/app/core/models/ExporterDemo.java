095182704bd31373cefc21780d0449e3bfdfab4e
Kunal <Kunal@Kunal-HP>
2018-04-10 23:32:08 +0530

    exporter demo

--- core/src/main/java/com/app/core/models/ExporterDemo.java
+++ core/src/main/java/com/app/core/models/ExporterDemo.java
@@ -2,13 +2,16 @@ package com.app.core.models;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.ExporterOption;
 import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class GenericModelDemo {
+@Model(adaptables = Resource.class, resourceType = { "aem63/components/content/allField" }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Exporter(name = "jackson", extensions = "json", options = { @ExporterOption(name = "SerializationFeature.WRITE_DATES_AS_TIMESTAMPS", value = "true") })
+public class ExporterDemo {
 
 	@Inject
 	String textArea;
@@ -60,4 +63,4 @@ public class GenericModelDemo {
 		return slingResourceType;
 	}
 
-}
+}	
